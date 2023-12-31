45cbdc05597cac27edf91e075b0380950b187230
Meonako <dubi.db28@gmail.com>
2023-12-31 13:44:02 +0700

    Feat: New output format

--- src/main.rs
+++ src/main.rs
@@ -12,23 +12,40 @@ fn main() {
     let start = u8::MIN;
     let end = u8::MAX;
 
+    let mut msg: HashMap<char, Vec<(char, char)>> = HashMap::new();
+
     for i in start..=end {
-        let mut msg = HashMap::new();
         for j in start..=end {
             let res = i ^ j;
             if want.contains(&res) {
-                msg.insert(res as char, format!(r#""{}" ^ "{}""#, i as char, j as char));
+                let vect = msg.get_mut(&(res as char));
+                match vect {
+                    Some(v) => {
+                        v.push((i as char, j as char));
+                    }
+                    None => {
+                        msg.insert(res as char, vec![(i as char, j as char)]);
+                    }
+                }
+                // msg.insert(res as char, format!(r#""{}" ^ "{}""#, i as char, j as char));
             }
         }
-        for c in want.iter() {
-            let msg = if msg.contains_key(&(*c as char)) {
-                format!("  {} :   {}", (*c as char), msg[&(*c as char)])
-            } else {
-                format!("  {} :   {}", (*c as char), "Not found")
-            };
+    }
 
-            println!("{:^width$}", msg, width = seperator.len());
-        }
-        println!("{}", seperator);
+    let mut lock = std::io::stdout().lock();
+    for c in want.iter() {
+        let outvec = msg.get(&(*c as char));
+        let output = match outvec {
+            Some(v) => {
+                let mut output = String::new();
+                for (i, j) in v {
+                    output.push_str(&format!("\n\t{:?} ^ {:?}", i, j));
+                }
+                format!("{} : {}", (*c as char), output)
+            }
+            None => format!("{} : Not found", (*c as char)),
+        };
+
+        writeln!(lock, "{:^width$}\n{}", output, seperator, width = seperator.len()).unwrap();
     }
 }
